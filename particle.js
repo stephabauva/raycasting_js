90a77f1d016ddb68ca3f3d174c9fc4a2a303a016
sb <stephane.baudry.vanaa@gmail.com>
2021-02-02 17:32:40 +0100

    with multiple walls

--- particle.js
+++ particle.js
@@ -1,9 +1,34 @@
 class Particle {
     constructor() {
-        this.pos = createVector(width / 2, height /2);
+        this.pos = createVector(width / 2, height / 2);
         this.rays = [];
-        for  (let a = 0; a < 360; a +=10) {
+        for (let a = 0; a < 360; a +=10) {
             this.rays.push(new Ray(this.pos, radians(a)));
+        }
+    }
+
+    update(x,y) {
+        this.pos.set(x,y);
+    }
+
+    look(walls) {
+        for (let ray of this.rays) {
+            let closest = null;
+            let record = Infinity;
+            for (let wall of walls) {
+                
+                const pt = ray.cast(wall);
+                if (pt) {
+                    const d = p5.Vector.dist(this.pos, pt);
+                    if (d < record) {
+                        record = d;
+                        closest = pt;
+                    }
+                }
+            }
+            if (closest) {
+                line(this.pos.x, this.pos.y, closest.x, closest.y);
+            }
         }
     }
 
--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 let ray;
-let wall;
+let particle;
+let walls = [];
 
 function setup() {
     createCanvas(400, 400);
-    wall = new Boundary(300, 100, 300, 300); /*from Boundary.js*/
+    // wall = new Boundary(300, 100, 300, 300); /*from Boundary.js*/
     // ray = new Ray(100, 200); /*from ray.js*/
+    for (let i = 0; i < 5; i++) {
+        let x1 = random(width);
+        let x2 = random(width);
+        let y1 = random(height);
+        let y2 = random(height);
+        walls[i] = new Boundary(x1, y1, x2, y2);
+    }
     particle = new Particle();
 }
 
 function draw() {
     background(0);
-    wall.show();
+    for (let wall of walls) {
+        wall.show();
+    }
     particle.show();
+    particle.update(mouseX, mouseY);
+    particle.look(walls);
     // ray.show();
     // ray.lookAt(mouseX, mouseY);
 
